3e9474db2d028cb94806526960629dba2c4a3d74
Petr Muntyanov <muntianovpv@mail.ru>
2023-03-12 12:55:32 +0400

    task64

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число N:");
+string first = Console.ReadLine();
+string last = ("1");
+Console.WriteLine(NaturalNumbersString(first, last));
+
+string NaturalNumbersString(string start, string end)
+{
+    if (start == end)
+    {
+        return start + ".";
+    }
+    else
+    {
+        return start + "," + NaturalNumbersString(Convert.ToString(int.Parse(start) - 1), end);
+    }
+}
